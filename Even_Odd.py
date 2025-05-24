d7b6b7a5c12669b224c7602c9e224bf789618f78
Ayyan927 <princeayyandxb@gmail.com>
2025-05-24 17:39:57 +0500

    add files

---
+++ Even_Odd.py
@@ -0,0 +1,26 @@
+# Given an array of integers.
+
+# Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+# If the input is an empty array or is null, return an empty array.
+
+# Example
+# For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+
+# solution
+def count_positives_sum_negatives(array):
+    if not array:
+        return []
+    count_positives = 0
+    sum_negatives = 0
+    for num in array:
+        if num > 0:
+            count_positives += 1
+        elif num < 0:
+            sum_negatives += num
+    return [count_positives, sum_negatives]
+
+num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]
+result = count_positives_sum_negatives(num_list)
+print(result)
